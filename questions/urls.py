e1a1d45379368a47d645abc56e69281cc3a0a732
Sarumantw <syed216645@gmail.com>
2025-02-09 11:02:49 +0300

    Update urls.py
    Add path to next question view

--- questions/urls.py
+++ questions/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('allquestions/<str:subject>', views.quiz_view, name='quiz_view'),
+    path('quiz/<str:subject>', views.quiz_view, name='quiz_view'),
     path('check_answers/', views.check_answers, name='check_answers'),
+    path('quiz/<str:subject>/next/<int:qid>/', views.next_question, name= 'nextq'),
 ]
