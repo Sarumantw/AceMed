c044e7479c6419802fbf8560f5effb1398dd5934
Sarumantw <syed216645@gmail.com>
2025-02-09 10:52:22 +0300

    Update views.py
    Modify views to change quiz to step by step

--- questions/views.py
+++ questions/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import question
+from django.http import JsonResponse
 # Create your views here.
 def quiz_view(request, subject):
-    questions = question.objects.filter(subject=subject)
-    return render(request, 'allqs.html', {'questions': questions, 'subject': subject})
+    first_question = question.objects.filter(subject=subject).first()
+    return render(request, 'allqs.html', {'questions': first_question, 'subject': subject})
 def check_answers(request):
     if request.method == 'POST':
         questions = question.objects.all()
@@ -14,3 +15,17 @@ def check_answers(request):
         return render(request, 'results', {'score': score})
 def homepage(request):
     return render(request, 'main.html')
+    def next_question(request, subject, q_id):
+        next_q = question.objects.filter(subject = subject, id__gt = q_id).first()
+        if next_q:
+            return JsonResponse({
+                'id' : next_q.id,
+                'question' : next_q.text,
+                'optiona' : next_q.option1,
+                'optionb' : next_q.option2,
+                'optionc' : next_q.option3,
+                'optiond' : next_q.option4,
+                'correct' : next_q.correct })
+        else: 
+           return JsonResponse({
+               'end' : True })
